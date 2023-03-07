83050b0c64c6ba0b6bd00574f59a836c7f632aac
psong1 <paulsong29@gmail.com>
2023-03-06 21:18:56 -0500

    moved onto project section

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+
+const printForecast = arr => {
+    let string = '';
+    for (let i = 0; i < arr.length; i++) {
+        string += `${arr[i]} in ${i + 1} days ...`;
+    }
+    console.log('...' + string);
+}
+
+printForecast(data1);
