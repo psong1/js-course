2fa595397db963c893847bbd07b90a667bb083e0
psong1 <paulsong29@gmail.com>
2023-04-11 09:42:21 -0400

    finished challenge 2

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -98,3 +98,38 @@ for (const [team, odd] of Object.entries(game.odds)) {
     const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}:`
     console.log(`Odd of ${teamStr}: ${odd}`);
 }
+
+
+// Challenge 3
+const gameEvents = new Map([
+    [17, 'GOAL'],
+    [36, 'SUBSTITUTION'],
+    [47, 'GOAL'],
+    [61, 'SUBSTITUTION'],
+    [64, 'YELLOW CARD'],
+    [69, 'RED CARD'],
+    [70, 'SUBSTITUTION'],
+    [72, 'SUBSTITUTION'],
+    [76, 'GOAL'],
+    [80, 'GOAL'],
+    [92, 'YELLOW CARD']
+]);
+console.log(gameEvents.keys());
+console.log(gameEvents.values());
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+
+console.log(gameEvents.delete(64));
+const eventsArr = [...gameEvents];
+
+const avgArr = 90 / eventsArr.length;
+console.log(avgArr);
+
+console.log(`"An event happened, on
+average, every ${avgArr} minutes`);
+
+for (const [key, value] of gameEvents) {
+    if (key < 45) console.log(`FIRST HALF ${key}: ${value}`);
+    else console.log(`SECOND HALF ${key}: ${value}`); 
+}
+
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,37 +50,70 @@ const restaurant = {
     openingHours,
   };
 
- const rest = new Map();
- rest.set('name', 'Classico Italiano');
- rest.set(1, 'Firenze, Italy');
- console.log(rest.set(2, 'Lisbon'));
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try Again!']
+]);
+console.log(question);
 
- rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
- .set('open', 11)
- .set('close', 23)
- .set(true, 'We are open')
- .set(false, 'We are closed')
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
- console.log(rest.get('name'));
- console.log(rest.get(true));
- console.log(rest.get(1));
+// Quiz app
+console.log((question.get('question')));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+};
+const answer = Number(prompt('Your answer'));
+console.log(answer);
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(question.get(question.get('correct') === answer));
 
-console.log(rest.has('categories'));
-rest.delete(2);
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+//  const rest = new Map();
+//  rest.set('name', 'Classico Italiano');
+//  rest.set(1, 'Firenze, Italy');
+//  console.log(rest.set(2, 'Lisbon'));
+
+//  rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//  .set('open', 11)
+//  .set('close', 23)
+//  .set(true, 'We are open')
+//  .set(false, 'We are closed')
+
+//  console.log(rest.get('name'));
+//  console.log(rest.get(true));
+//  console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
 // rest.clear();
-const arr = [1, 2];
-rest.set(arr, 'Test')
-rest.set(document.querySelector('h1'), 'Heading');
+// const arr = [1, 2];
+// rest.set(arr, 'Test')
+// rest.set(document.querySelector('h1'), 'Heading');
 
-console.log(rest);
-console.log(rest.size);
+// console.log(rest);
+// console.log(rest.size);
 
 // return undefined bc array is not the same object in the heap
 // console.log(rest.get([1, 2]));
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
   // const ordersSet = new Set ([
   //   'Pasta',
